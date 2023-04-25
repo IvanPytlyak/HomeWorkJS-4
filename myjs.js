a984a6075fc8c069d486856ec699324bc3695c41
Ivan Pytlyak <mario38671@gmail.com>
2023-04-25 23:10:21 +0300

    changes

--- myjs.js
+++ myjs.js
@@ -1,15 +1,12 @@
-const data = []; // речь на занятии шла о том, что мы не должны выносить переменные за предел функций как в данном случае этого избежать?;
-document.querySelector("#submit").addEventListener("click", (event) => {
-  event.preventDefault();
-  const list = {
-    title: document.querySelector("#title").value,
-    description: document.querySelector("#description").value,
-  };
-  data.push(list);
+function createForm() {
+  const data = [];
 
-  document.querySelector(".todoList").innerHTML = "";
-  data.forEach((item, index) => {
-    document.querySelector(".todoList").innerHTML += `
+  const toDoList = document.querySelector(".todoList");
+
+  function render() {
+    toDoList.innerHTML = "";
+    data.forEach((item, index) => {
+      toDoList.innerHTML += `
               <div class="wrapper">
               №: ${index + 1}<br>
               <div class="tit">Title:${item.title}</div><br>
@@ -18,27 +15,30 @@ document.querySelector("#submit").addEventListener("click", (event) => {
               <button class="deleteButton">Delete</button>\n
               </div>
           `;
+    });
+  }
+
+  function addItem(event) {
+    event.preventDefault();
+    const list = {
+      title: document.querySelector("#title").value,
+      description: document.querySelector("#description").value,
+    };
+    data.push(list);
+    render();
+  }
+
+  document.querySelector("#submit").addEventListener("click", (event) => {
+    addItem(event);
   });
-});
 
-document.querySelector(".todoList").addEventListener("click", (event) => {
-  const myDelBtn = event.target.closest(".deleteButton");
-  const wrapper = myDelBtn.closest(".wrapper");
-  const index = Array.from(wrapper.parentElement.children).indexOf(wrapper);
-  data.splice(index, 1);
-  wrapper.remove();
-});
-
-// document.querySelector(".editButton").addEventListener("click", (event) => {
-//   const wrapper = event.target.closest(".wrapper");
-//   const title = listItem.querySelector(".tit");
-//   const description = listItem.querySelector(".desc");
-
-//   const titleInput = document.createElement("input");
-//   titleInput.classList = "edit_title";
-//   titleInput.type = "text";
-//   titleInput.value = document.querySelector(".tit").replace("Title: ", "");
-//   const makeEditButtton = document.createElement("input");
-//   titleInput.type = "button";
-//   makeEditButtton.classList = "make_edit_button";
-// });
+  toDoList.addEventListener("click", (event) => {
+    const myDelBtn = event.target.closest(".deleteButton");
+    const wrapper = myDelBtn.closest(".wrapper");
+    const index = Array.from(wrapper.parentElement.children).indexOf(wrapper);
+    data.splice(index, 1);
+    wrapper.remove();
+    // render(); // рендеринг не соблюдает нумерацию как на предыдущей строке
+  });
+}
+createForm();
